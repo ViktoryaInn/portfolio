a22d0f6f8ededc3c8909b714b0549e6b1f729812
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-11-25 17:51:59 +0300

    some changes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import "../css/main.css";
-
 let menu = document.querySelector('.header__menu');
 let hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle("header__menu--active");
+    hamburger.classList.toggle("hamburger--active");
 })
