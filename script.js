a03e32f42fb280b51bad72dd535e538789fbfcee
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-11-27 02:16:30 +0300

    some changes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let menu = document.querySelector('.header__menu');
 let hamburger = document.querySelector('.hamburger');
+let writeButton = document.querySelector('.footer__write');
+let modal = document.querySelector('.modal__window');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle("header__menu--active");
     hamburger.classList.toggle("hamburger--active");
-})
+});
+
+writeButton.addEventListener('click', () => {
+    modal.classList.toggle("modal__window--active");
+});
