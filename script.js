f43a38dfdd65e7aa6daec6b69e1e0d2ccd07217f
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-11-28 03:08:02 +0300

    modal window for desktop

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let menu = document.querySelector('.header__menu');
 let hamburger = document.querySelector('.hamburger');
 let writeButton = document.querySelector('.footer__write');
 let modal = document.querySelector('.modal__window');
+let closeButton = document.querySelector('.modal__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle("header__menu--active");
@@ -10,4 +11,14 @@ hamburger.addEventListener('click', () => {
 
 writeButton.addEventListener('click', () => {
     modal.classList.toggle("modal__window--active");
+});
+
+closeButton.addEventListener('click', () => {
+    modal.classList.toggle("modal__window--active");
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target == modal){
+        modal.classList.toggle("modal__window--active");
+    }
 });
